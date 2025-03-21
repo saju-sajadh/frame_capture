b37577f239cbfff0a0b9cb094f4214b421c2859c
saju-sajadh <sajadhtechosa@gmail.com>
2025-03-21 17:52:15 +0530

    'ok'

--- src/pages/emolyzer.jsx
+++ src/pages/emolyzer.jsx
@@ -22,22 +22,22 @@ const Emolyzer = () => {
           href="/"
           className="font-bold italic text-3xl text-[#1A73E8] font-mono tracking-widest cursor-pointer"
         >
-          Emolyzer
+          Frame Capture
         </a>
         <p className="bg-[#1A73E8] text-white rounded px-3 py-2 font-bold hover:bg-blue-600">
-          hello
+          welcome
         </p>
       </nav>
       <div className="grid lg:grid-cols-2 gap-10 lg:gap-40 w-full h-screen px-4 md:px-10 lg:px-40">
         <div className="flex flex-col lg:justify-center lg:items-end justify-end items-center text-center lg:text-left">
           <p className="text-[#1A73E8] text-[36px] md:text-[48px] lg:text-[64px] font-bold">
-            Emotion Analyzer
+            Frame - Capture
           </p>
           <p className="text-[18px] md:text-[24px] lg:text-[28px] font-bold text-black max-w-[500px] m-1">
-            Analyze Emotions of your patients Live from anywhere.
+            Analyze Emotions of users Live from anywhere.
           </p>
           <p className="m-1 text-[14px] md:text-[16px] lg:text-[18px] text-black max-w-[500px]">
-            A fast, easy way to collect facial expression data, from anywhere in
+            A fast, easy way to collect sentimental facial expression data, from anywhere in
             the world.
           </p>
           <div className="w-[500px]">
@@ -45,7 +45,7 @@ const Emolyzer = () => {
               onClick={handleSignout}
               className="bg-[#1A73E8] mt-4 text-white rounded px-6 py-3 font-bold hover:bg-blue-600"
             >
-              Signout
+              Go Back
             </button>
           </div>
         </div>
